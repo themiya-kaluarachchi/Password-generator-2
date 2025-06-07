c371ce4800b8dd5e494f1876423d12787ed4ffdc
Themiya <themiyakaluarachchi@gmail.com>
2025-06-08 02:04:04 +0530

    init

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -14,7 +14,17 @@ function App() {
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
         <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'></div>
-        <input type="text" />
+        <input 
+          type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='Password'
+          readOnly
+        />
+
+        <button
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >Copy</button>
 
       </div>
     </>
