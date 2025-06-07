f1573db30a3231abbd0634decfa10f1bfe38c885
Themiya <themiyakaluarachchi@gmail.com>
2025-06-08 03:02:50 +0530

    feat: Add display password input feild

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -13,8 +13,9 @@ function App() {
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
         <h1 className='text-white text-center my-3'>Password Generator</h1>
-        <div className='flex shadow rounded-lg overflow-hidden mb-4'></div>
-        <input 
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+
+          <input 
           type="text"
           value={password}
           className='outline-none w-full py-1 px-3'
@@ -22,10 +23,17 @@ function App() {
           readOnly
         />
 
-        <button
+          <button
           className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
-        >Copy</button>
-
+          >Copy</button>
+        </div>
+        <div
+        className='flex text-sm gap-x-1'>
+          <div className='flex items-center gap-x-1'>
+            <input type="text" name='' id=''/>
+          </div>
+          
+        </div>
       </div>
     </>
   )
