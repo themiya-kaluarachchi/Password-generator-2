f5008465d45c67a044c85fd44bf90637c968eeb5
Themiya <themiyakaluarachchi@gmail.com>
2025-06-08 03:53:46 +0530

    feat: create func and use hooks

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,7 +9,26 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState('')
 
-  
+const generatePassword = useCallback(() => {
+  let pass = ""
+  let str =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+  if(numberAllowed) str += "0123456789"
+  if(charAllowed) str += "!@#$%^&*()_+"
+    
+  for(let i = 1; i < length; i++)
+  {
+    const char = Math.floor(Math.random() * str.length + 1)
+    pass += str.charAt(char)
+  }
+
+  setPassword(pass)
+}, [length, numberAllowed, charAllowed])
+   
+  useEffect (() => {
+    generatePassword()
+  }, [length, numberAllowed, charAllowed])
 
   return (
     <>
